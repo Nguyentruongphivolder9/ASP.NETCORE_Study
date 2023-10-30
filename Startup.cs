e47afecaabe549d84d87fe87f57d34676a9aff38
phi_ironman <nguyentruongphi15032003@gmail.com>
2023-10-30 16:47:46 +0700

    session AddDistributedMemoryCache

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,12 @@ namespace App.NetCore_Study
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession((option) =>
+            {
+                option.Cookie.Name = "philogic";
+                option.IOTimeout = new TimeSpan(0, 30, 0);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +32,8 @@ namespace App.NetCore_Study
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSession(); //SessionMiddleware
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -33,6 +41,23 @@ namespace App.NetCore_Study
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                });
+
+                endpoints.MapGet("/readWriteSession", async context =>
+                {
+                    int? count;
+                    count = context.Session.GetInt32("count"); // read session
+
+                    if (count == null)
+                    {
+                        count = 0;
+                    }
+
+                    count += 1;
+
+                    context.Session.SetInt32("count", count.Value); // save session
+
+                    await context.Response.WriteAsync($"Số lần truy cập readWriteSession là: {count}");
                 });
             });
         }
